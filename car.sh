e856c5101005c65f3152cf4aa3111355edc3797c
Oscar <69419493+oscarsangwin@users.noreply.github.com>
2021-02-22 17:05:15 +0000

    Updated car.sh -> error handling for if code doesn't compile

--- car.sh
+++ car.sh
@@ -2,25 +2,28 @@
 
 if [[ $# -eq 1 ]]
 then
-	file=$1
-	output_filename='car-executable'
+        file=$1
+        output_filename='car-executable'
 
-	echo "Compiling executable as car-file from: $(pwd)/$file..."
-	gcc -o $output_filename $file
+        echo "Compiling executable as car-file from: $(pwd)/$file..."
+        if gcc -o $output_filename $file
+        then
+                echo $'Finished compiling successfully, now running...\n'
+                ./$output_filename #&&
 
-	echo $'Finished compiling, now running...\n'
-	./$output_filename #&&
-	
-	# Optional output at the end (the '&&' above not needed if not included):
-	echo $'\nFinished. The executable will not be deleted'
+                # Optional output at the end (the '&&' above not needed if not included):
+                echo $'\nFinished. The executable will not be deleted'
 
-	# FOLLOWING CODE REMOVED AS 'rm car-file' COULD BE DANGEROUS
-	# echo '' # Newline
-	# echo "Finished, now deleting executable: $output_filename..."
-	# rm $output_filename
-	# echo $'Done!'
+                # FOLLOWING CODE REMOVED AS 'rm car-file' COULD BE DANGEROUS
+                # echo '' # Newline
+                # echo "Finished, now deleting executable: $output_filename..."
+                # rm $output_filename
+                # echo $'Done!'
+        else
+                echo $'\nDid not compile successfully, there is no executable to run.'
+        fi
 
 else
-	echo 'Incorrect number of params provided'
-	echo 'The car command, or Compile And Run, will use gcc to compile and run an executable'
+        echo 'Incorrect number of params provided'
+        echo 'The car command, or Compile And Run, will use gcc to compile and run an executable'
 fi
